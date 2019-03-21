a1c85fc4d5c4fdf026638b3abf5088a5aac31bb6
Steven Z <stevencsu@163.com>
2019-03-21 23:43:44 +0800

    add javascript demo

---
+++ js-example/scripts/main.js
@@ -0,0 +1,34 @@
+/*
+Everything in between is a comment.
+*/
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-1.jpg') {
+        myImage.setAttribute('src', 'images/firefox-2.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-1.jpg')
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
